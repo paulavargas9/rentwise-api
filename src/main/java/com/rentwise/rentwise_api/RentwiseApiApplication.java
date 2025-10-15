e9f85923cc2bc8f21ceea678df5978788f3357eb
paulavargas9 <paulavargasilva09@gmail.com>
2025-10-15 22:43:00 +0200

    : bootstrap project (skeleton + config/dev/test)

---
+++ src/main/java/com/rentwise/rentwise_api/RentwiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.rentwise.rentwise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentwiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentwiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentwise/rentwise_api/RentwiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentwise.rentwise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentwiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
